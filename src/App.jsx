2e3180e2dfa8d6f2201ca20e5ba4390d13165727
khubaib004 <khabbab607342@gmail.com>
2023-09-28 13:28:48 +0600

    Handle form with onSubmit and access field data

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Simpleform from './components/Simpleform/Simpleform'
+
+function App() {
+
+    return (
+        <>
+            <Simpleform></Simpleform>
+        </>
+    )
+}
+
+export default App
